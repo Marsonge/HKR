602eba50e5e8a93d165fb5d4161c7c6b8b0ee6e3
Fabien Bal-Fontaine <fabien_bf@hotmail.fr>
2017-08-07 19:27:44 +0100

    We now have a domain service layer

--- HKRCore/DTO/Coord.cs
+++ HKRCore/DTO/Coord.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace HKRWebServices.DTO
+namespace HKRCore.DTO
 {
     public class Coord
     {
---
+++ HKRCore/Exception/DomainException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HKRCore.Exception
+{
+    public class DomainException : System.Exception
+    {
+        public DomainException() { }
+        public DomainException( string message ) : base( message ) { }
+        public DomainException( string message, System.Exception inner ) : base( message, inner ) { }
+    }
+
+}
